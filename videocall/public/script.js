516450512f1bcb4f12af5cc9366cf5814fe8dbb8
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2024-04-20 13:06:12 +0200

    video conference setup

--- videocall/public/script.js
+++ videocall/public/script.js
@@ -11,18 +11,28 @@ navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addVideoStream(myVideo, stream);
+    addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call =>{
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream =>{
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId =>{
+        connectToNewUser(userId, stream)
+    })
 }).catch(error => {
     console.error('Error accessing media devices:', error);
     // Handle error (e.g., show an error message to the user)
 });
 
+
+
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
-});
-
-socket.on('user-connected', userId => {
-    console.log('User connected: ' + userId);
 });
 
 function addVideoStream(video, stream) {
@@ -31,4 +41,15 @@ function addVideoStream(video, stream) {
         video.play();
     });
     videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = documents.createElement('video') 
+    call.on('stream', userVideoStream =>{        
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        VideoColorSpace.remove()
+    })
 }
