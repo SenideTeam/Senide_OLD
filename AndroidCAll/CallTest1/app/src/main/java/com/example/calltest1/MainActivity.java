4136fe6b03fd1ee3226829d519644995e76f7013
calo.gerson <calo.gerson@uni.eus>
2024-05-10 10:52:14 +0200

    call

--- AndroidCAll/CallTest1/app/src/main/java/com/example/calltest1/MainActivity.java
+++ AndroidCAll/CallTest1/app/src/main/java/com/example/calltest1/MainActivity.java
@@ -9,42 +9,30 @@ import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.Manifest;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CALL_PHONE = 1;
+    private static final int REQUEST_READ_PHONE_STATE = 2;
     private long startTime; // Tiempo de inicio de la llamada
-    private boolean callEnded = false; // Para verificar si la llamada ha terminado
+    private boolean callEnded = true; // Para verificar si la llamada ha terminado
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-        telephonyManager.listen(new PhoneStateListener() {
-            @Override
-            public void onCallStateChanged(int state, String incomingNumber) {
-                super.onCallStateChanged(state, incomingNumber);
-                if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
-                    startTime = System.currentTimeMillis(); // Registrar el tiempo de inicio
-                }
-                if (state == TelephonyManager.CALL_STATE_IDLE && !callEnded) {
-                    long endTime = System.currentTimeMillis();
-                    long elapsedTime = endTime - startTime;
-                    if (elapsedTime < 15000) { // Menos de 15 segundos
-                        Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
-                        startActivity(webIntent);
-                    }
-                    callEnded = true;
-                } else {
-                    callEnded = false;
-                }
-            }
-        }, PhoneStateListener.LISTEN_CALL_STATE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+        } else {
+            initTelephonyManager();
+        }
 
         setupImageView(findViewById(R.id.imageView1), "684263667"); // Número específico para imageView1
         setupImageView(findViewById(R.id.imageView2), "662204776"); // Número general para otras imágenes
@@ -60,12 +48,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initTelephonyManager() {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        // La llamada ha comenzado
+                        startTime = System.currentTimeMillis();
+                        callEnded = false; // Restablecer el estado de finalización de la llamada
+                        break;
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        if (!callEnded) {
+                            long endTime = System.currentTimeMillis();
+                            long elapsedTime = endTime - startTime;
+                            // Verificar si la llamada fue menor a 15 segundos
+                            if (elapsedTime < 15000) {
+                                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                                startActivity(webIntent);
+                            }
+                            callEnded = true; // Indicar que la llamada ha terminado
+                        }
+                        break;
+                }
+            }
+        }, PhoneStateListener.LISTEN_CALL_STATE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_READ_PHONE_STATE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initTelephonyManager();
+            } else {
+                Toast.makeText(this, "Permission denied to read phone state", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private void setupImageView(ImageView imageView, String phoneNumber) {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:" + phoneNumber));
+                finish();
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
                     startActivity(callIntent);
                 } else {
